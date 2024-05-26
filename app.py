6a1611f9f5c9e8da3ff77eae3eb5f80cb3e0e522
RawPikachu <102355977z@gmail.com>
2024-05-26 15:12:08 -0400

    integrations

--- app.py
+++ app.py
@@ -1,36 +1,46 @@
 from flask import Flask, render_template, Response
+import numpy as np
+import torch
 import cv2
+import time
+
+camera = cv2.VideoCapture(0)
+while not camera.isOpened():
+    time.sleep(0.1)
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)  # use 0 for web camera
-#  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
-# for local webcam use cv2.VideoCapture(0)
+model = torch.hub.load("ultralytics/yolov5", "custom", path="models/best.pt")
+
 
 def gen_frames():  # generate frame by frame from camera
     while True:
         # Capture frame-by-frame
         success, frame = camera.read()  # read the camera frame
+
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
+            results = model(frame)
+            label, box = results.xyxyn[0][:, -1].numpy(), results.xyxyn[0][:, :-1].numpy()
+            ret, buffer = cv2.imencode(".jpg", np.squeeze(results.render()))
             frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+            yield (
+                b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+            )  # concat frame one by one and show result
 
 
-@app.route('/video_feed')
+@app.route("/video_feed")
 def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
-@app.route('/')
+@app.route("/")
 def index():
     """Video streaming home page."""
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=False, use_reloader=False)
